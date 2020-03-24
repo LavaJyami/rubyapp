9bbb598ca898c4d3af447bdc68695d1038a422c6
Madhav Acharya <ankur@stems-solutions.com>
2020-03-24 08:03:08 +0545

    snapshot test added to the wordlist component

--- reactfrontend/src/Components/Wordlist/__tests__/wordlist.test.js
+++ reactfrontend/src/Components/Wordlist/__tests__/wordlist.test.js
@@ -5,6 +5,8 @@ import Wordlist from '../Wordlist';
 import { render, cleanup } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
 
+import renderer from 'react-test-renderer';
+
 afterEach(cleanup);
 
 var wordlistArr = ["apple", "oranges", "game", "pc"]
@@ -13,8 +15,12 @@ it("Word list renders without crashing",() => {
   ReactDOM.render(<Wordlist value={wordlistArr} />, div);
 });
 
-
 it("Words in the word list component are rendered properly",() => {
   const {getByTestId} = render(<Wordlist value = {wordlistArr}/>);
   expect (getByTestId('wordlist')).toHaveTextContent("apple");
+});
+
+it("matches snapshot",  () => {
+  const tree = renderer.create(<Wordlist value = {wordlistArr} />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
