5d74370cee487945d2b8c66b5b5b29de1735a424
Madhav Acharya <ankur@stems-solutions.com>
2020-03-24 20:52:11 +0545

    fixed the DFS issue

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::BoardsController < ApplicationController
   # GET /boards
   def index
    # vowels_def = ['a','e','i','o','u']
-   common_letter_def = ['e','a','r','i','o','t','n','s','l','c']
-   render json: [[common_letter_def.to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample , ('a'..'z').to_a.sample],[('a'..'z').to_a.sample, common_letter_def.to_a.sample, ('a'..'z').to_a.sample, common_letter_def.to_a.sample],[('a'..'z').to_a.sample, common_letter_def.to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample],[common_letter_def.to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample]]
+   # common_letter_def = ['e','a','r','i','o','t','n','s','l','c']
+   # render json: [[common_letter_def.to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample , ('a'..'z').to_a.sample],[('a'..'z').to_a.sample, common_letter_def.to_a.sample, ('a'..'z').to_a.sample, common_letter_def.to_a.sample],[('a'..'z').to_a.sample, common_letter_def.to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample],[common_letter_def.to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample]]
+   render json: [["x","s","o","e"],["f","m","r","t"],["y","o","r","u"],["e","g","q","y"]]
   end
   end
--- reactfrontend/src/Components/Gameboard/Gameboard.js
+++ reactfrontend/src/Components/Gameboard/Gameboard.js
@@ -36,7 +36,6 @@ class GameBoard extends Component {
 
   testing(){
     this.validateWordAPI_test("test");
-    console.log("testingAPI");
   }
 
   handleChange(event){
@@ -111,7 +110,8 @@ class GameBoard extends Component {
         const key = 'dict.1.1.20200319T090129Z.8eb6b755e125a705.7fd9b9cb85a09a0dd9c47a86bb564c856893cafc';
         const response = await fetch(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${key}&lang=en-ru&text=${word}`);
         const data = await response.json();
-        console.log(data);
+        console.log('API: ');
+        console.log(data && data.def.length>0);
         if(data && data.def.length>0){
           if(this.validate(word) === true ) {
               this.setState({error: ''});
@@ -143,6 +143,8 @@ class GameBoard extends Component {
       for(let i=0;i<board.length;i++){
           for(let j=0;j<board[0].length;j++){
               if(board[i][j].toLowerCase() === currentWord[0]){
+                console.log('result of dfs');
+                console.log(this.depthFirstSearch(i,j,board,currentWord));
               if(this.depthFirstSearch(i, j, board, currentWord))
               return true;
             }
@@ -156,79 +158,105 @@ class GameBoard extends Component {
     let approvedLetters = [[i,j]];
     const wordLength = word.length;
     const boardDim = board.length;
-    var k = 1;
-    var value = [];
-    var foundFlag  = false;
-    var lettersFound = 0;
-    var unchecked_branch = 0;
+    let k = 1;
+    let value = [];
+    let foundFlag  = false;
+    let lettersFound = 0;
+    let unchecked_branch = 0;
 
     while(k<=wordLength){
-
       if(!this.isOutOfBounds(i,j,boardDim-1,'tp') && !this.isRepeated(approvedLetters,i-1,j) && board[i-1][j].toLowerCase()===word[k]){
-        stack.push([i-1,j]); foundFlag = true; lettersFound++;
+        stack.push([i-1,j]); foundFlag = true; lettersFound++; console.log('found in tp');
       }
-      if(!this.isOutOfBounds(i,j,boardDim-1,'tr') && !this.isRepeated(approvedLetters,i-1,j) && board[i-1][j+1].toLowerCase()===word[k]){
-        stack.push([i-1,j+1]);  foundFlag = true; lettersFound++;
+      if(!this.isOutOfBounds(i,j,boardDim-1,'tr') && !this.isRepeated(approvedLetters,i-1,j+1) && board[i-1][j+1].toLowerCase()===word[k]){
+        stack.push([i-1,j+1]);  foundFlag = true; lettersFound++; console.log('found in tr');
       }
       if(!this.isOutOfBounds(i,j,boardDim-1,'rt') && !this.isRepeated(approvedLetters,i,j+1) && board[i][j+1].toLowerCase()===word[k]){
-        stack.push([i,j+1]);  foundFlag = true; lettersFound++;
+        stack.push([i,j+1]);  foundFlag = true; lettersFound++; console.log('found in rt');
         }
       if(!this.isOutOfBounds(i,j,boardDim-1,'br') && !this.isRepeated(approvedLetters,i+1,j+1) && board[i+1][j+1].toLowerCase()===word[k]){
-        stack.push([i+1,j+1]); foundFlag = true; lettersFound++;
+        stack.push([i+1,j+1]); foundFlag = true; lettersFound++; console.log('found in br');
         }
       if(!this.isOutOfBounds(i,j,boardDim-1,'bt') && !this.isRepeated(approvedLetters,i+1,j) && board[i+1][j].toLowerCase()===word[k]){
-        stack.push([i+1,j]);  foundFlag = true; lettersFound++;
+        stack.push([i+1,j]);  foundFlag = true; lettersFound++; console.log('found in bt');
         }
       if(!this.isOutOfBounds(i,j,boardDim-1,'bl') && !this.isRepeated(approvedLetters,i+1,j-1) && board[i+1][j-1].toLowerCase()===word[k]){
-        stack.push([i+1,j-1]); foundFlag = true; lettersFound++;
+        stack.push([i+1,j-1]); foundFlag = true; lettersFound++; console.log('found in bl');
         }
       if(!this.isOutOfBounds(i,j,boardDim-1,'lt') && !this.isRepeated(approvedLetters,i,j-1) && board[i][j-1].toLowerCase()===word[k]){
-        stack.push([i,j-1]); foundFlag = true; lettersFound++;
+        stack.push([i,j-1]); foundFlag = true; lettersFound++; console.log('found in lt');
         }
       if(!this.isOutOfBounds(i,j,boardDim-1,'tl') && !this.isRepeated(approvedLetters,i-1,j-1) && board[i-1][j-1].toLowerCase()===word[k]){
-        stack.push([i-1,j-1]); foundFlag = true; lettersFound++;
+        stack.push([i-1,j-1]); foundFlag = true; lettersFound++; console.log('found in tl');
         }
 
         if(!foundFlag && stack.length === 0) {
             return false;
         }
-
         if(foundFlag && lettersFound === 1){
-          k++;
-          if(k === wordLength)
-          return true;
-          value = stack.pop();i = value[0]; j = value[1]; foundFlag=false; lettersFound = 0;
-          approvedLetters.push(value);
+            k++;
+            if(k === wordLength)
+            return true;
+            value = stack.pop();i = value[0]; j = value[1]; foundFlag=false; lettersFound = 0;
+
+            approvedLetters.push(value);
+            console.log(approvedLetters);
+            for(let x=0; x<approvedLetters.length; x++){
+              let a = approvedLetters[x][0];
+              let b = approvedLetters[x][1];
+              console.log(board[a][b]);
+            }
         }
          else if(foundFlag && lettersFound> 1){
-          unchecked_branch = k+1;
-          k++;
-          if(k === wordLength)
-          return true;
-          value = stack.pop();i = value[0]; j = value[1]; foundFlag=false; lettersFound = 0;
-          approvedLetters.push(value);
+            unchecked_branch = k+1;
+            k++;
+            if(k === wordLength)
+            return true;
+            value = stack.pop();i = value[0]; j = value[1]; foundFlag=false; lettersFound = 0;
+            approvedLetters.push(value);
+            console.log(approvedLetters);
+            for(let x=0; x<approvedLetters.length; x++){
+              let a = approvedLetters[x][0];
+              let b = approvedLetters[x][1];
+              console.log(board[a][b]);
+            }
+            console.log('value of k (found multiple): '+ k);
+
         }
          else if(!foundFlag && stack.length>0){
-          k = unchecked_branch;
-          const number_of_items_to_backtrack = approvedLetters.length - (k-1);
-          for(let l=0;l<number_of_items_to_backtrack;l++){
-            approvedLetters.pop();
-          }
-          value = stack.pop();i = value[0]; j = value[1]; lettersFound = 0;
-          approvedLetters.push(value);
+            k = unchecked_branch;
+            const number_of_items_to_backtrack = approvedLetters.length - (k-1);
+            console.log('unchecked branch');
+            console.log('---------------------------------------');
+            console.log(approvedLetters);
+            for(let x=0; x<approvedLetters.length; x++){
+              let a = approvedLetters[x][0];
+              let b = approvedLetters[x][1];
+              console.log(board[a][b]);
+            }
+            for(let l=0;l<number_of_items_to_backtrack;l++){
+              approvedLetters.pop();
+            }
+            value = stack.pop();i = value[0]; j = value[1]; lettersFound = 0;
+            approvedLetters.push(value);
+            console.log(approvedLetters);
+            for(let x=0; x<approvedLetters.length; x++){
+              let a = approvedLetters[x][0];
+              let b = approvedLetters[x][1];
+              console.log(board[a][b]);
+            }
+            console.log('value of k back_tracking: '+ k);
+
         }
       }
   }
 
   secondsToTime(secs){
       let hours = Math.floor(secs / (60 * 60));
-
       let divisor_for_minutes = secs % (60 * 60);
       let minutes = Math.floor(divisor_for_minutes / 60);
-
       let divisor_for_seconds = divisor_for_minutes % 60;
       let seconds = Math.ceil(divisor_for_seconds);
-
       let obj = {
         "h": hours,
         "m": minutes,
@@ -244,14 +272,11 @@ class GameBoard extends Component {
   }
 
   countDown() {
-    // Remove one second, set state so a re-render happens.
     let seconds = this.state.seconds - 1;
     this.setState({
       time: this.secondsToTime(seconds),
       seconds: seconds,
     });
-
-    // Check if we're at zero.
     if (seconds === 0) {
       this.endGame();
       clearInterval(this.timer);
@@ -262,11 +287,12 @@ class GameBoard extends Component {
   async startGame() {
     const response = await fetch('/api/v1/boards');
     const data = await response.json();
+    let timeLeftVar = this.secondsToTime(this.state.seconds);
     this.setState({
       board: data,
-      approvedWords: []});
-    let timeLeftVar = this.secondsToTime(this.state.seconds);
-    this.setState({ time: timeLeftVar });
+      approvedWords: [],
+      time: timeLeftVar
+    });
     this.startTimer();
   }
 
@@ -301,7 +327,6 @@ class GameBoard extends Component {
     return(
       <div id="gameboard" >
             <div className = "boards">
-
                 <div className = "section_first">
                     <Grid value = {board}/>
                     <form id="input_box" onSubmit={this.validateWordAPI}>
@@ -319,21 +344,19 @@ class GameBoard extends Component {
                     </form>
                     <p className = "error">{error}</p>
                     <div className = "playAgain">
-                      <button
-                      onClick={this.playAgain}
-                      >Play Next >>
-                      </button>
+                        <button
+                        onClick={this.playAgain}
+                        >Play Next >>
+                        </button>
                     </div>
                 </div>
-
                 <div className = "section_second">
                     <div id="timer">
-                      Time remaining: {time.m} : {time.s}
+                        Time remaining: {time.m} : {time.s}
                     </div>
                     <Wordlist value = {approvedWords}/>
                     <Score words={approvedWords} />
                 </div>
-
             </div>
       </div>
 
