50f4bda34c7fe91c81e2172e00092804aefac386
Madhav Acharya <madhav@Madhavs-MacBook-Air.local>
2020-03-24 06:45:58 +0545

    minor spelling changes

--- reactfrontend/src/Components/Wordlist/__tests__/wordlist.test.js
+++ reactfrontend/src/Components/Wordlist/__tests__/wordlist.test.js
@@ -8,13 +8,13 @@ import "@testing-library/jest-dom/extend-expect";
 afterEach(cleanup);
 
 var wordlistArr = ["apple", "oranges", "game", "pc"]
-it("Wordlist renders without crashing",() => {
+it("Word list renders without crashing",() => {
   const div = document.createElement("div");
   ReactDOM.render(<Wordlist value={wordlistArr} />, div);
 });
 
 
-it("Words in the wordlist component are rendered correctly",() => {
+it("Words in the word list component are rendered correctly",() => {
   const {getByTestId} = render(<Wordlist value = {wordlistArr}/>);
   expect (getByTestId('wordlist')).toHaveTextContent("apple");
 });
