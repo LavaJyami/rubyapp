9b14c71b15b79aade3a93b694a1018c25c98e144
Madhav Acharya <ankur@stems-solutions.com>
2020-03-24 18:25:46 +0545

    changed the use of vowels to use of commonly used letters in English language

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::BoardsController < ApplicationController
   before_action :set_board, only: [:show, :update, :destroy]
   # GET /boards
   def index
-   # @boards = Board.all
-   vowels_def = ['a','e','i','o','u']
-   render json: [[('a'..'z').to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample , ('a'..'z').to_a.sample],[('a'..'z').to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample],[('a'..'z').to_a.sample, vowels_def.to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample],[vowels_def.to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample]]
+   # vowels_def = ['a','e','i','o','u']
+   common_letter_def = ['e','a','r','i','o','t','n','s','l','c']
+   render json: [[common_letter_def.to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample , ('a'..'z').to_a.sample],[('a'..'z').to_a.sample, common_letter_def.to_a.sample, ('a'..'z').to_a.sample, common_letter_def.to_a.sample],[('a'..'z').to_a.sample, common_letter_def.to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample],[common_letter_def.to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample, ('a'..'z').to_a.sample]]
   end
   end
